7b87aae8fda387259c37d6debdbb3cb293d0f381
HuseyinSaidKoca <huseyinsaidk@gmail.com>
2023-09-19 05:56:08 +0300

    services page added

---
+++ services_page.js
@@ -0,0 +1,214 @@
+// JavaScript ile butonlara tıklama olaylarını ekleyin
+window.addEventListener("DOMContentLoaded", (event) => {//sayfa yüklenmesini bekle
+    document.getElementById("description-1").style.display = "flex";
+    const box1 = document.getElementById("box-1");
+    const box2 = document.getElementById("box-2");
+    const box3 = document.getElementById("box-3");
+    const box4 = document.getElementById("box-4");
+    const box5 = document.getElementById("box-5");
+    const box6 = document.getElementById("box-6");
+    const box7 = document.getElementById("box-7");
+    const box8 = document.getElementById("box-8");
+
+    const mobilebox1 = document.getElementById("mobile-box-1");
+    const mobilebox2 = document.getElementById("mobile-box-2");
+    const mobilebox3 = document.getElementById("mobile-box-3");
+    const mobilebox4 = document.getElementById("mobile-box-4");
+    const mobilebox5 = document.getElementById("mobile-box-5");
+    const mobilebox6 = document.getElementById("mobile-box-6");
+    const mobilebox7 = document.getElementById("mobile-box-7");
+    const mobilebox8 = document.getElementById("mobile-box-8");
+
+    box1.addEventListener("click", function (e) {
+    
+    e.preventDefault(); // Sayfa yenilemeyi engellemek için
+    document.querySelectorAll(".team-text").forEach(function (div) {
+        div.style.display = "none";
+    });
+
+    // Buton 1'e ait açıklamayı göster
+    document.getElementById("description-1").style.display = "flex";
+    // Tıklama işlemi için burada yapmak istediğiniz işlemi gerçekleştirin
+    });
+
+    box2.addEventListener("click", function (e) {
+    
+    e.preventDefault(); // Sayfa yenilemeyi engellemek için
+    document.querySelectorAll(".team-text").forEach(function (div) {
+        div.style.display = "none";
+    });
+
+    
+    document.getElementById("description-2").style.display = "flex";
+    // Tıklama işlemi için burada yapmak istediğiniz işlemi gerçekleştirin
+    });
+
+    box3.addEventListener("click", function (e) {
+    
+    e.preventDefault(); // Sayfa yenilemeyi engellemek için
+    document.querySelectorAll(".team-text").forEach(function (div) {
+        div.style.display = "none";
+    });
+
+    
+    document.getElementById("description-3").style.display = "flex";
+    // Tıklama işlemi için burada yapmak istediğiniz işlemi gerçekleştirin
+    });
+
+    box4.addEventListener("click", function (e) {
+    
+    e.preventDefault(); // Sayfa yenilemeyi engellemek için
+    document.querySelectorAll(".team-text").forEach(function (div) {
+        div.style.display = "none";
+    });
+
+    
+    document.getElementById("description-4").style.display = "flex";
+    // Tıklama işlemi için burada yapmak istediğiniz işlemi gerçekleştirin
+    });
+
+    box5.addEventListener("click", function (e) {
+    
+    e.preventDefault(); // Sayfa yenilemeyi engellemek için
+    document.querySelectorAll(".team-text").forEach(function (div) {
+        div.style.display = "none";
+    });
+
+    
+    document.getElementById("description-5").style.display = "flex";
+    // Tıklama işlemi için burada yapmak istediğiniz işlemi gerçekleştirin
+    });
+
+    box6.addEventListener("click", function (e) {
+    
+    e.preventDefault(); // Sayfa yenilemeyi engellemek için
+    document.querySelectorAll(".team-text").forEach(function (div) {
+        div.style.display = "none";
+    });
+
+    
+    document.getElementById("description-6").style.display = "flex";
+    // Tıklama işlemi için burada yapmak istediğiniz işlemi gerçekleştirin
+    });
+
+    box7.addEventListener("click", function (e) {
+    
+    e.preventDefault(); // Sayfa yenilemeyi engellemek için
+    document.querySelectorAll(".team-text").forEach(function (div) {
+        div.style.display = "none";
+    });
+
+    
+    document.getElementById("description-7").style.display = "flex";
+    // Tıklama işlemi için burada yapmak istediğiniz işlemi gerçekleştirin
+    });
+
+    box8.addEventListener("click", function (e) {
+    
+    e.preventDefault(); // Sayfa yenilemeyi engellemek için
+    document.querySelectorAll(".team-text").forEach(function (div) {
+        div.style.display = "none";
+    });
+
+    
+    document.getElementById("description-8").style.display = "flex";
+    // Tıklama işlemi için burada yapmak istediğiniz işlemi gerçekleştirin
+    });
+
+    //MOBİL İÇİN AÇIKLAMA KISMI
+    mobilebox1.addEventListener("click", function (e) {
+    
+    e.preventDefault(); // Sayfa yenilemeyi engellemek için
+    document.querySelectorAll(".team-text").forEach(function (div) {
+        div.style.display = "none";
+    });
+
+    
+    document.getElementById("description-1").style.display = "flex";
+    // Tıklama işlemi için burada yapmak istediğiniz işlemi gerçekleştirin
+    });
+
+    mobilebox2.addEventListener("click", function (e) {
+    
+    e.preventDefault(); // Sayfa yenilemeyi engellemek için
+    document.querySelectorAll(".team-text").forEach(function (div) {
+        div.style.display = "none";
+    });
+
+    
+    document.getElementById("description-2").style.display = "flex";
+    // Tıklama işlemi için burada yapmak istediğiniz işlemi gerçekleştirin
+    });
+
+    mobilebox3.addEventListener("click", function (e) {
+    
+    e.preventDefault(); // Sayfa yenilemeyi engellemek için
+    document.querySelectorAll(".team-text").forEach(function (div) {
+        div.style.display = "none";
+    });
+
+    
+    document.getElementById("description-3").style.display = "flex";
+    // Tıklama işlemi için burada yapmak istediğiniz işlemi gerçekleştirin
+    });
+
+    mobilebox4.addEventListener("click", function (e) {
+    
+    e.preventDefault(); // Sayfa yenilemeyi engellemek için
+    document.querySelectorAll(".team-text").forEach(function (div) {
+        div.style.display = "none";
+    });
+
+    
+    document.getElementById("description-4").style.display = "flex";
+    // Tıklama işlemi için burada yapmak istediğiniz işlemi gerçekleştirin
+    });
+
+    mobilebox5.addEventListener("click", function (e) {
+    
+    e.preventDefault(); // Sayfa yenilemeyi engellemek için
+    document.querySelectorAll(".team-text").forEach(function (div) {
+        div.style.display = "none";
+    });
+
+    
+    document.getElementById("description-5").style.display = "flex";
+    // Tıklama işlemi için burada yapmak istediğiniz işlemi gerçekleştirin
+    });
+
+    mobilebox6.addEventListener("click", function (e) {
+    
+    e.preventDefault(); // Sayfa yenilemeyi engellemek için
+    document.querySelectorAll(".team-text").forEach(function (div) {
+        div.style.display = "none";
+    });
+
+    
+    document.getElementById("description-6").style.display = "flex";
+    // Tıklama işlemi için burada yapmak istediğiniz işlemi gerçekleştirin
+    });
+
+    mobilebox7.addEventListener("click", function (e) {
+    
+    e.preventDefault(); // Sayfa yenilemeyi engellemek için
+    document.querySelectorAll(".team-text").forEach(function (div) {
+        div.style.display = "none";
+    });
+
+    
+    document.getElementById("description-7").style.display = "flex";
+    // Tıklama işlemi için burada yapmak istediğiniz işlemi gerçekleştirin
+    });
+
+    mobilebox8.addEventListener("click", function (e) {
+    
+    e.preventDefault(); // Sayfa yenilemeyi engellemek için
+    document.querySelectorAll(".team-text").forEach(function (div) {
+        div.style.display = "none";
+    });
+
+    
+    document.getElementById("description-8").style.display = "flex";
+    // Tıklama işlemi için burada yapmak istediğiniz işlemi gerçekleştirin
+    });
+});
