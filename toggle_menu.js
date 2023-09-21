fa91f16dc6b014031c4ac254655f5d3274de4d8a
HuseyinSaidKoca <huseyinsaidk@gmail.com>
2023-09-21 20:15:48 +0300

    HEADER AND FOOTER ADDED TO CONTACT, NO SCROLL ADDED

--- toggle_menu.js
+++ toggle_menu.js
@@ -6,10 +6,11 @@ const closeIcon = document.getElementById("closeIcon");
 
 menuIcon.addEventListener("click", function () {
     toggleMenu.style.right = "0"; // Menüyü sağdan sola kaydır
+    document.body.classList.add('no-scroll'); // Kaydırmayı engelle
 });
-
 closeIcon.addEventListener("click", function () {
     toggleMenu.style.right = "-43vw"; // Menüyü tekrar sağa kaydır ve gizle
+    document.body.classList.remove('no-scroll'); // Kaydırmayı tekrar etkinleştir
 });
 
 })
