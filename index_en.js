fdcd173d5c079fd2f24fa016c83bcecaacc29b1b
HuseyinSaidKoca <huseyinsaidk@gmail.com>
2023-09-23 02:50:14 +0300

    English Translation

---
+++ index_en.js
@@ -0,0 +1,13 @@
+window.addEventListener("DOMContentLoaded", (event) => {//sayfa yüklenmesini bekle
+const ctaButton = document.querySelector(".cta-button");
+
+ctaButton.addEventListener("click", function() {
+    window.location.href = "contact_page_en.html";
+});
+
+const aboutButton = document.querySelector(".about-us-button");
+
+aboutButton.addEventListener("click", function() {
+    window.location.href = "about_us_page_en.html";
+});
+})
